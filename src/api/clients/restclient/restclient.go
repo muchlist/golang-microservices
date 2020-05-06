8d7722d7c2d72aca0224832eb28556aaa824bff2
mrtsdc <whois.muchlis@gmail.com>
2020-05-06 15:55:53 +0800

    membuat testing http Client

--- src/api/clients/restclient/restclient.go
+++ src/api/clients/restclient/restclient.go
@@ -3,10 +3,57 @@ package restclient
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
 )
 
+var (
+	enableMock = false
+	mocks      = make(map[string]*Mock)
+)
+
+//Mock is struct yang digunakan untuk keperluan Mocking response
+type Mock struct {
+	URL        string
+	HTTPMethod string
+	Response   *http.Response
+	Error      error
+}
+
+func getMockID(httpMethod string, url string) string {
+	return fmt.Sprintf("%s_%s", httpMethod, url)
+}
+
+//StartMockUp membuat enableMock menjadi true untuk keperluan testing
+func StartMockUp() {
+	enableMock = true
+}
+
+//FlushMockups is membuat nilai mock pada test sebelumnya menjadi mock kosong
+func FlushMockups() {
+	mocks = make(map[string]*Mock)
+}
+
+//StopMockUp membuat enableMock menjadi false
+func StopMockUp() {
+	enableMock = false
+}
+
+//AddMockUp menambahkan mock file untuk testing
+func AddMockUp(mock Mock) {
+	mocks[getMockID(mock.HTTPMethod, mock.URL)] = &mock
+}
+
+//Post membuat repo github
 func Post(url string, body interface{}, headers http.Header) (*http.Response, error) {
+	if enableMock {
+		mock := mocks[getMockID(http.MethodPost, url)]
+		if mock == nil {
+			return nil, errors.New("no mockup found for give request")
+		}
+		return mock.Response, mock.Error
+	}
 
 	jsonBytes, err := json.Marshal(body)
 	if err != nil {
--- src/api/providers/githubprovider/github_provider_test.go
+++ src/api/providers/githubprovider/github_provider_test.go
@@ -1,12 +1,140 @@
 package githubprovider
 
 import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
 	"testing"
+
+	"github.com/muchlist/golang-microservices/src/api/clients/restclient"
+	"github.com/muchlist/golang-microservices/src/api/domain/github"
 
 	"github.com/stretchr/testify/assert"
 )
 
+//TestMain mengaktifkan mock dalam setiap testing
+func TestMain(m *testing.M) {
+	restclient.StartMockUp()
+	os.Exit(m.Run())
+}
+
 func TestGetAuthorizationHeader(t *testing.T) {
 	header := getAuthorizationHeader("abc123")
 	assert.EqualValues(t, "token abc123", header)
+}
+
+func TestCreateRepoErrorRestclient(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockUp(restclient.Mock{
+		URL:        "https://api.github.com/user/repos",
+		HTTPMethod: http.MethodPost,
+		Error:      errors.New("invalid restclient response"),
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequest{})
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, "invalid restclient response", err.Message)
+}
+
+func TestCreateRepoErrorInvalidResponseBody(t *testing.T) {
+	restclient.FlushMockups()
+	invalidCloser, _ := os.Open("-asf3")
+	restclient.AddMockUp(restclient.Mock{
+		URL:        "https://api.github.com/user/repos",
+		HTTPMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       invalidCloser,
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequest{})
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
+	assert.EqualValues(t, "invalid response body", err.Message)
+}
+
+func TestCreateRepoErrorInvalidErrorInterface(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockUp(restclient.Mock{
+		URL:        "https://api.github.com/user/repos",
+		HTTPMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusUnauthorized,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"message"}:1`)),
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequest{})
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
+	assert.EqualValues(t, "invalid json response body", err.Message)
+}
+
+func TestCreateRepoUnauthorized(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockUp(restclient.Mock{
+		URL:        "https://api.github.com/user/repos",
+		HTTPMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusUnauthorized,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"message": "Requires authentication","documentation_url": "https://developer.github.com/v3/repos/#create"}`)),
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequest{})
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusUnauthorized, err.StatusCode)
+	assert.EqualValues(t, "Requires authentication", err.Message)
+}
+
+func TestCreateRepoInvalidSuccessResponse(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockUp(restclient.Mock{
+		URL:        "https://api.github.com/user/repos",
+		HTTPMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"id": "123"}`)),
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequest{})
+
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.StatusCode)
+	assert.EqualValues(t, "error when trying to unmarshalling github create repo response", err.Message)
+}
+
+func TestCreateRepoNoError(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockUp(restclient.Mock{
+		URL:        "https://api.github.com/user/repos",
+		HTTPMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"id": 123,"name": "golang-tutorial","full_name": "muchlis/golang-tutorial"}`)),
+		},
+	})
+
+	response, err := CreateRepo("", github.CreateRepoRequest{})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, response)
+	assert.EqualValues(t, 123, response.ID)
+	assert.EqualValues(t, "golang-tutorial", response.Name)
+	assert.EqualValues(t, "muchlis/golang-tutorial", response.FullName)
+}
+
+func TestConstant(t *testing.T) {
+	assert.EqualValues(t, "Authorization", headerAuthorization)
 }
