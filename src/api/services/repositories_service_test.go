4b96f4d0d63678ee10dbfb1e4d5d333bf891efeb
mrtsdc <whois.muchlis@gmail.com>
2020-05-10 22:43:29 +0800

    testing repository service 100

---
+++ src/api/services/repositories_service_test.go
@@ -0,0 +1,72 @@
+package services
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/muchlist/golang-microservices/src/api/clients/restclient"
+	"github.com/muchlist/golang-microservices/src/api/domain/repositories"
+)
+
+func TestMain(m *testing.M) {
+	restclient.StartMockUp()
+	os.Exit(m.Run())
+}
+
+func TestCreateRepoInvalidInputName(t *testing.T) {
+	request := repositories.CreateRepoRequest{}
+	result, err := RepositoryService.CreateRepo(request)
+
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusBadRequest, err.Status())
+	assert.EqualValues(t, "invalid repository name", err.Message())
+}
+
+func TestCreateRepoErrorFromGithub(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockUp(restclient.Mock{
+		URL:        "https://api.github.com/user/repos",
+		HTTPMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusUnauthorized,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"message": "Requires authentication","documentation_url": "https://developer.github.com/docs"}`)),
+		},
+	},
+	)
+
+	request := repositories.CreateRepoRequest{Name: "testing"}
+	result, err := RepositoryService.CreateRepo(request)
+
+	assert.Nil(t, result)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusUnauthorized, err.Status())
+	assert.EqualValues(t, "Requires authentication", err.Message())
+
+}
+
+func TestCreateRepoNoError(t *testing.T) {
+	restclient.FlushMockups()
+	restclient.AddMockUp(restclient.Mock{
+		URL:        "https://api.github.com/user/repos",
+		HTTPMethod: http.MethodPost,
+		Response: &http.Response{
+			StatusCode: http.StatusCreated,
+			Body:       ioutil.NopCloser(strings.NewReader(`{"id": 123,"name": "golang-tutorial","full_name": "muchlis/golang-tutorial"}`)),
+		},
+	},
+	)
+
+	request := repositories.CreateRepoRequest{Name: "testing"}
+	result, err := RepositoryService.CreateRepo(request)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.EqualValues(t, 123, result.ID)
+	assert.EqualValues(t, "golang-tutorial", result.Name)
+}
